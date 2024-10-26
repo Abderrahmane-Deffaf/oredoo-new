96972c3c146ae944ea4470dd4b5e8cb8426e7ad6
D-Abderrahmane <deabdou55@gmail.com>
2024-10-26 16:17:33 +0100

    commit new project

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import logo from "@/assets/logo-ooredoo.png";
 export default function Logo() {
   return (
     <div className=" w-1/2 md:w-1/3 lg:w-1/5 ">
-      <Image src={logo} alt="oredoo logo " />
+      <Image src={logo} alt="oredoo logo company " />
     </div>
   );
 }
